97842437d96a855d0cfd9f9131049de7ec41054c
meen0806 <meen0806@gmail.com>
2025-03-03 20:35:21 +0530

    worked on search-campaigns

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
 
-router.get('/', accountController.listAccounts);
+router.get('/list', accountController.listAccounts);
 router.post('/', accountController.createAccount);
 router.get('/:id', accountController.getAccountById);
 router.put('/:id', accountController.updateAccount);
 router.delete('/:id', accountController.deleteAccount);
 router.post('/verify', accountController.verifyAccount);
+router.get('/', accountController.srchAccounts);
 
 module.exports = router;
